4465deae44d7efb407ebbb1bc12bb08e041aa8ff
Carlos <carlos@walcu.com>
2020-10-12 02:41:51 +0200

    Updates

---
+++ precache-manifest.04de0a432397b3f10834101367336790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c617f7262086ec97da16bfb21b57ee4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca66a0f6236a36de5cce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58302575035a3f124497",
+    "url": "/static/js/2.0ee8efc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ee8efc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca66a0f6236a36de5cce",
+    "url": "/static/js/main.11e80d4a.chunk.js"
+  },
+  {
+    "revision": "f07f37da0d73cb549c6c",
+    "url": "/static/js/runtime-main.11281f0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
